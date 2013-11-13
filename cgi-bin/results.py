a68afe3cce71d9b9516741c69567320c1807383f
brianh014 <brianh014@gmail.com>
2013-11-13 17:15:17 -0600

    script update

--- cgi-bin/results.py
+++ cgi-bin/results.py
@@ -12,9 +12,28 @@ query = form.getvalue('query')
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
+print """
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="Keywords" content="wikipedia,wikibot,wiki,bot,reddit">
+	<meta name="Description" content="Site for the Reddit bot WikiBot">
+    
+    <link rel="stylesheet" type="text/css" href="style.css">
+"""
+
+
 print "<title>WikiBot Results</title>"
 print "</head>"
 print "<body>"
+
+print """
+<div class="navbar">
+        <div style="margin-left:15px; padding-top:10px; font-size:25px; font-family:Arial;">
+            <a href="index.html">Home</a>
+            <a href="stats.html">Stats</a>
+            <a href="faq.html">FAQ</a>
+        </div>
+    </div> 
+"""
 print "<h2>This is your query: </h2>" % (query)
 print "</body>"
 print "</html>"
