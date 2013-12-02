e7949eb72db5d1824d9e5a1f37115bc7e8278c58
brianh014 <brianh014@gmail.com>
2013-12-01 23:06:36 -0600

    encoding

--- cgi-bin/results.py
+++ cgi-bin/results.py
@@ -14,7 +14,7 @@ if len(sys.argv) > 1:
     query = sys.argv[1]
     lang = sys.argv[2]
 else:
-    query = unidecode(form.getvalue('query'))
+    query = unidecode(form['query'].value)
     lang = form.getvalue('language')
 
 #get wiki page
